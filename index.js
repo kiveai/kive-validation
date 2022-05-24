f3488a3d63dc4ce5e3d97933af49147cc5de23bd
Martin <martinagnalv@gmail.com>
2022-05-24 14:14:27 +0200

    correct build dir

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 "use strict";
-exports.__esModule = true;
-exports.validateJobTitle = exports.validateBio = exports.validateEmail = exports.validateLastName = exports.validateFirstName = void 0;
-var yup = require("yup");
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validateJobTitle = exports.validateBio = exports.validateEmail = exports.validateLastName = exports.validateFirstName = exports.Constants = void 0;
+var yup = __importStar(require("yup"));
+var consts_1 = require("./consts");
+exports.Constants = {
+    FIRST_NAME_MAX_LENGTH: consts_1.FIRST_NAME_MAX_LENGTH,
+    FIRST_NAME_MIN_LENGTH: consts_1.FIRST_NAME_MIN_LENGTH,
+    LAST_NAME_MAX_LENGTH: consts_1.LAST_NAME_MAX_LENGTH,
+    LAST_NAME_MIN_LENGTH: consts_1.LAST_NAME_MIN_LENGTH,
+    BIO_MAX_LENGTH: consts_1.BIO_MAX_LENGTH,
+};
 var validateFirstName = function (firstName) {
     return yup.string()
-        .min(2, 'First name must be at least 2 characters long')
-        .max(30, 'First name must be less than 30 characters long')
+        .min(consts_1.FIRST_NAME_MIN_LENGTH, 'First name must be at least 2 characters long')
+        .max(consts_1.FIRST_NAME_MAX_LENGTH, 'First name must be less than 30 characters long')
         .validate(firstName)
         .then(function () { return true; });
 };
 exports.validateFirstName = validateFirstName;
 var validateLastName = function (lastName) {
     return yup.string()
-        .min(2, 'Last name must be at least 2 characters long')
-        .max(30, 'Last name must be less than 30 characters long')
+        .min(consts_1.LAST_NAME_MIN_LENGTH, 'Last name must be at least 2 characters long')
+        .max(consts_1.LAST_NAME_MAX_LENGTH, 'Last name must be less than 30 characters long')
         .validate(lastName)
         .then(function () { return true; });
 };
@@ -27,7 +54,7 @@ var validateEmail = function (email) {
 exports.validateEmail = validateEmail;
 var validateBio = function (bio) {
     return yup.string()
-        .max(255, 'Bio must be less than 255 characters long')
+        .max(consts_1.BIO_MAX_LENGTH, 'Bio must be less than 255 characters long')
         .validate(bio)
         .then(function () { return true; });
 };
