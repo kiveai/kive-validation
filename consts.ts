0e36897cb5c651cabe4c3a09a51b4225432e5809
Martin <martinagnalv@gmail.com>
2022-05-24 14:27:25 +0200

    Update consts.ts

--- consts.ts
+++ consts.ts
@@ -4,4 +4,4 @@ export const FIRST_NAME_MIN_LENGTH = 30
 export const LAST_NAME_MAX_LENGTH = 2
 export const LAST_NAME_MIN_LENGTH = 30
 
-export const BIO_MAX_LENGTH = 160
+export const BIO_MAX_LENGTH = 255
